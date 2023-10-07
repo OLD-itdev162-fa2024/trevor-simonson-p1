397a943c7eec97357e0ca5e665b21dc2605bac34
Trevor Simonson <simonsot@gmatc.matc.edu>
2023-10-07 11:48:41 -0500

    Basic Frontend Complete

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,10 +1,26 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+
+export class AppComponent implements OnInit{
   title = 'client';
+  nasaPotds: any;
+
+  constructor(private http: HttpClient){
+
+  }
+
+  ngOnInit(): void {
+    this.http.get('http://localhost:5175/nasapotd/getall').subscribe(
+      response => {this.nasaPotds = response},
+      error => { console.log(error) }
+    )
+
+  }
 }
+
